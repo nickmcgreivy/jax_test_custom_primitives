de008263a2c629a9f3b5532f88e7c072c011ceb2
Nick McGreivy <mcgreivy@princeton.edu>
2022-03-02 00:05:03 -1000

    Sparse solve prim implemented

--- sparse_solve_prim/sparse_solve.cpp
+++ sparse_solve_prim/sparse_solve.cpp
@@ -1,46 +1,75 @@
-#include<iostream>
-#include<Eigen/Sparse>
-#include<Eigen/Dense>
 #include<Eigen/SparseCholesky>
 #include<pybind11/eigen.h>
-
-using SparseM = Eigen::SparseMatrix<double>;
-using Eigen::Ref;
-using Eigen::VectorXd;
-using Eigen::MatrixXd;
-
+#include<pybind11/pybind11.h>
+#include<pybind11/eigen.h>
+#include<Eigen/Dense>
+#include<Eigen/Sparse>
+#include<iostream>
+#include<cstdint>
 namespace py = pybind11;
+using Eigen::Map;
+using Eigen::MatrixXi;
+using Eigen::Matrix;
+using Eigen::Dynamic;
+using Eigen::VectorX;
 
-class SparseSolver {
-public:
-	SparseSolver(const SparseM A) : A(A) {
-		solver.analyzePattern(A);
-		solver.factorize(A);
+template <typename T>
+void sparse_solve(void* out_ptr, void** data_ptr) {
+	T* b_ptr = reinterpret_cast<T *>(data_ptr[0]);
+  T* sparse_data_ptr = reinterpret_cast<T *>(data_ptr[1]);
+  int* sparse_indices_ptr = reinterpret_cast<int *>(data_ptr[2]);    
+  const int N_global = *reinterpret_cast<const int *>(data_ptr[3]);
+  const std::int64_t V_size = *reinterpret_cast<const std::int64_t *>(data_ptr[4]);
+  const bool forward = *reinterpret_cast<const bool *>(data_ptr[5]);
+  VectorX<T> V_data = Map<const VectorX<T>>(sparse_data_ptr, V_size);
+  MatrixXi V_indices = Map<const MatrixXi>(sparse_indices_ptr, V_size, 2);
+
+  static int prev_N_global = 0;
+  static Eigen::SimplicialLDLT<Eigen::SparseMatrix<T>> forwardsolver;
+  static Eigen::SimplicialLDLT<Eigen::SparseMatrix<T>> backwardsolver;
+
+  if (N_global == prev_N_global) { 
+  	// do nothing
+  } else {
+	  // create matrix, create solver, and analyze it
+	  std::vector<Eigen::Triplet<T>> tripletList;
+	  tripletList.reserve(V_size);
+	  for (int i = 0; i < V_size; ++i) {
+	    tripletList.push_back(Eigen::Triplet<T>(V_indices(i,0),V_indices(i,1),V_data(i)));
+	  }
+	  Eigen::SparseMatrix<T> V(N_global,N_global);
+	  V.setFromTriplets(tripletList.begin(), tripletList.end());
+
+	  prev_N_global = N_global;
+
+	  forwardsolver.analyzePattern(V);
+		forwardsolver.factorize(V);
+
+		backwardsolver.analyzePattern(V.transpose());
+		backwardsolver.factorize(V.transpose());
 	}
 
-	SparseSolver(const SparseSolver& s) {
-		A = s.get_matrix();
-		solver.analyzePattern(A);
-		solver.factorize(A);
-	}
-
-	VectorXd solve(const VectorXd b) {
-		return solver.solve(b);
-	}
-
-private:
-
-	SparseM get_matrix() const {
-		return A;
-	}
-
-	SparseM A;
-	Eigen::SimplicialLDLT<SparseM> solver;
-};
+  VectorX<T> b = Map<const VectorX<T>>(b_ptr,N_global);
+  T* x_ptr = reinterpret_cast<T *>(out_ptr);
+  if (forward) {
+  	Map<VectorX<T>>(x_ptr, N_global) = forwardsolver.solve(b);
+  } else {
+  	Map<VectorX<T>>(x_ptr, N_global) = backwardsolver.solve(b);
+  }
+}
 
 
-PYBIND11_MODULE(eigensolve, m) {
-    py::class_<SparseSolver>(m, "SparseSolver")
-    	.def(py::init<const SparseM>())
-        .def("solve", &SparseSolver::solve);
+
+template <typename T>
+pybind11::capsule EncapsulateFunction(T* fn) {
+  return pybind11::capsule((void*)fn, "xla._CUSTOM_CALL_TARGET");
+}
+pybind11::dict Registrations() {
+  pybind11::dict dict;
+  dict["sparse_solve_f32"] = EncapsulateFunction(sparse_solve<float>);
+  dict["sparse_solve_f64"] = EncapsulateFunction(sparse_solve<double>);
+  return dict;
+}
+PYBIND11_MODULE(custom_call_sparse_solve, m) { 
+	m.def("registrations", &Registrations); 
 }
--- sparse_solve_prim/sparse_solve.py
+++ sparse_solve_prim/sparse_solve.py
@@ -1,5 +1,5 @@
 import jax
-from jax import lax, core, config
+from jax import lax, core, config, xla, jit, grad
 from jax._src import api, abstract_arrays
 from jax.interpreters import ad
 import jax.numpy as jnp
@@ -8,63 +8,84 @@ from jax.scipy.linalg import lu_solve
 from scipy.linalg import lu_solve as np_lu_solve
 from scipy.sparse import csr_matrix, csc_matrix
 from scipy.sparse.linalg import splu
+from jaxlib import xla_client
 
-import eigensolve
 
 sparse_solve_p = core.Primitive("sparse_solve")
 
+def sparse_solve_prim(b, sparse_data, sparse_indices, N_global, forward=True):
+    return sparse_solve_p.bind(b, sparse_data, sparse_indices, N_global, forward)
 
-def sparse_solve_prim(b, solver=None, solver_T=None):
-    return sparse_solve_p.bind(b, solver=solver, solver_T=solver_T)
+def sparse_solve_impl(b, sparse_data, sparse_indices, N_global, forward=True):
+    raise Exception("Sparse solve prim shouldn't be called except from within JIT")
+
+def sparse_solve_abstract_eval(b, sparse_data, sparse_indices, N_global, forward=True):
+    return abstract_arrays.ShapedArray(b.shape, b.dtype)
+
+def sparse_solve_value_and_jvp(primals, tangents):
+    (b, sparse_data, sparse_indices, N_global, forward) = primals
+    (bt, _, _, _, _) = tangents
+    primal_out = sparse_solve_prim(b, sparse_data, sparse_indices, N_global, forward=forward)
+    output_tangent = sparse_solve_prim(bt, sparse_data, sparse_indices, N_global, forward=forward)
+    return (primal_out, output_tangent)
+
+def sparse_solve_transpose(ct, b, sparse_data, sparse_indices, N_global, forward=True):
+    return (sparse_solve_prim(-ct, sparse_data, sparse_indices, N_global, forward=not forward), None, None, None, None)
 
 
-def sparse_solve_impl(b, solver=None, solver_T=None):
-    return jnp.asarray(solver.solve(np.asarray(b)))
+import custom_call_sparse_solve
+
+for _name, _value in custom_call_sparse_solve.registrations().items():
+    xla_client.register_cpu_custom_call_target(_name, _value)
+
+def sparse_solve_xla_translation(c, bc, sparse_data, sparse_indices, N_global, forward):
+    bc_shape = c.get_shape(bc)
+    bc_dtype = bc_shape.element_type()
+    bc_dims = bc_shape.dimensions()
+    bc_shape = xla_client.Shape.array_shape(jnp.dtype(bc_dtype), bc_dims, (0,))
+    out_shape = xla_client.Shape.array_shape(jnp.dtype(bc_dtype), bc_dims, (0,))
+    data_shape = c.get_shape(sparse_data)
+    data_dtype = data_shape.element_type()
+    data_dims = data_shape.dimensions()
+    data_shape = xla_client.Shape.array_shape(jnp.dtype(data_dtype), data_dims, (0,))
+    indices_shape = c.get_shape(sparse_indices)
+    indices_dtype = indices_shape.element_type()
+    indices_dims = indices_shape.dimensions()
+    indices_shape = xla_client.Shape.array_shape(jnp.dtype(indices_dtype), indices_dims, (0, 1))
+
+    assert bc_dtype == data_dtype
+
+    if bc_dtype == jnp.float32:
+        op_name = b"sparse_solve_f32"
+    elif bc_dtype == jnp.float64:
+        op_name = b"sparse_solve_f64"
+    else:
+        raise NotImplementedError(f"Unsupported dtype {bc_dtype}")
+    return xla_client.ops.CustomCallWithLayout(
+        c,
+        op_name,
+        operands=(bc,
+            sparse_data,
+            sparse_indices,
+            N_global,
+            xla_client.ops.ConstantLiteral(c, data_dims[0]),
+            forward,
+            ),
+        shape_with_layout=out_shape,
+        operand_shapes_with_layout=(bc_shape,
+            data_shape, 
+            indices_shape,
+            xla_client.Shape.array_shape(jnp.dtype(jnp.int64), (), ()),
+            xla_client.Shape.array_shape(jnp.dtype(jnp.int64), (), ()),
+            xla_client.Shape.array_shape(jnp.dtype(bool), (), ()),
+            ),
+    )
+
+
+xla.backend_specific_translations["cpu"][sparse_solve_p] = sparse_solve_xla_translation
 
 
 sparse_solve_p.def_impl(sparse_solve_impl)
-
-
-def sparse_solve_abstract_eval(b, solver=None, solver_T=None):
-    return abstract_arrays.ShapedArray(b.shape, b.dtype)
-
-
 sparse_solve_p.def_abstract_eval(sparse_solve_abstract_eval)
-
-
-def sparse_solve_value_and_jvp(primals, tangents, solver=None, solver_T=None):
-    (b,) = primals
-    (bt,) = tangents
-    primal_out = sparse_solve_prim(b, solver=solver, solver_T=solver_T)
-    output_tangent = sparse_solve_prim(bt, solver=solver, solver_T=solver_T)
-    return (primal_out, output_tangent)
-
-
 ad.primitive_jvps[sparse_solve_p] = sparse_solve_value_and_jvp
-
-
-def sparse_solve_transpose(ct, b, solver=None, solver_T=None):
-    return (sparse_solve_prim(-ct, solver=solver_T, solver_T=solver),)
-
-
 ad.primitive_transposes[sparse_solve_p] = sparse_solve_transpose
-
-
-V = jnp.asarray([[2.0, 0.0, 1.0], [1.0, -1.0, 0.0], [0.0, 1.0, -1.0]])
-b = jnp.asarray([1.0, 2.0, 3.0])
-bt = jnp.asarray([4.0, -1.0, 1.0])
-
-solver = eigensolve.SparseSolver(csr_matrix(V))
-solver_T = eigensolve.SparseSolver(csr_matrix(V.T))
-
-
-f = lambda b: jnp.sum(sparse_solve_prim(b, solver=solver, solver_T=solver_T) ** 2)
-f_grad = api.grad(f)
-print((sparse_solve_prim(b, solver=solver, solver_T=solver_T)))
-print(api.jvp(lambda b: sparse_solve_prim(b, solver=solver, solver_T=solver_T), (b,), (bt,)))
-print(f_grad(b))
-
-"""
-# GOAL IS TO GET THIS WORKING!
-print(f_jit(b))
-"""
